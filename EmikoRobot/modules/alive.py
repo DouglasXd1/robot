0f3b867215209660415f589531e8ac6f90de9858
DouglasXd1 <122727579+DouglasXd1@users.noreply.github.com>
2023-01-25 16:59:48 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -15,10 +15,10 @@ PHOTO = "https://telegra.ph/file/a9f2c0b24ea825378b683.jpg"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Douglas Robot.** \n\n"
   TEXT += " **I'm Working Properly** \n\n"
-  TEXT += f" **My Master : [Hisoka](https://t.me/hisoka69)** \n\n"
+  TEXT += f" **My Master : [razor](https://t.me/Rzrgnshn)** \n\n"
   TEXT += f" **Library Version :** `{telever}` \n\n"
   TEXT += f" **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f" **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here**"
-  BUTTON = [[Button.url("Help", "https://t.me/Douglasmusic_bot?start=help"), Button.url("Support", "https://t.me/Liveshowbanget")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Douglasmusic_bot?start=help"), Button.url("Support", "https://t.me/enamsembilanbanget")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
