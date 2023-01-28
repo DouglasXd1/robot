b86c7d1f088f3ecdb2528e3eb166907966ff32c3
DouglasXd1 <122727579+DouglasXd1@users.noreply.github.com>
2023-01-28 14:02:18 +0700

    Update forcesubs.py

--- EmikoRobot/modules/forcesubs.py
+++ EmikoRobot/modules/forcesubs.py
@@ -200,4 +200,4 @@ Feds are now divided into 3 sections for your ease.
 • `/fedadminhelp`*:* Provides help for fed administration commands
 • `/feduserhelp`*:* Provides help for commands anyone can use
 """
-__mod_name__ = "F-Sub/Feds"
+__mod_name__ = "ғsᴜʙ/ғᴇᴅs 🍭"
